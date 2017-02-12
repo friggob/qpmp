20bd22a4a8d13f727dfd30042c9c6566bedd9b7b
Fredrik Olausson <fredrik@bluppfisk.org>
2017-02-12 19:35:32 +0100

    Mostly done with UI stuff

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   qpmpWin w;
+  w.setFileList(a.arguments());
   w.show();
 
   return a.exec();
--- qpmpwin.h
+++ qpmpwin.h
@@ -13,10 +13,29 @@ class qpmpWin : public QMainWindow
 
   public:
 	explicit qpmpWin(QWidget *parent = 0);
+	void setFileList(QStringList);
+	QString sizeFormat(quint64 size);
 	~qpmpWin();
+
+  private slots:
+	void on_actionQuit_triggered();
+	void on_actionOpen_triggered();
+	void on_actionSaveAs_triggered();
+	void processFileList(QStringList);
+	void updateTable();
+
+	void on_actionRefresh_triggered();
 
   private:
 	Ui::qpmpWin *ui;
+	QStringList mFiles;
+	QStringList fList;
+	void resizeEvent(QResizeEvent *event);
+
+	void setupTable();
+
+  signals:
+	void mFilesUpdated();
 };
 
 #endif // QPMPWIN_H
